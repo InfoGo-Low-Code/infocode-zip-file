e5f657c70034911d46dae46d7443e465a8b01d58
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2025-07-28 14:12:03 -0300

    feat: change 'datetimeParserCustom' with date-fns-tz

--- src/utils/datetimeParserCustom.ts
+++ src/utils/datetimeParserCustom.ts
@@ -1,10 +1,15 @@
-import { formatDate } from 'date-fns'
 import { ptBR } from 'date-fns/locale/pt-BR'
+import { formatInTimeZone } from 'date-fns-tz'
 
 export function datetimeParserCustom(date: Date) {
-  const formattedDate = formatDate(date, 'dd/MM/yyyy HH:mm', {
-    locale: ptBR,
-  })
+  const formattedDate = formatInTimeZone(
+    date,
+    'America_Sao_Paulo',
+    'dd/MM/yyyy HH:mm',
+    {
+      locale: ptBR,
+    },
+  )
 
   return formattedDate
 }
