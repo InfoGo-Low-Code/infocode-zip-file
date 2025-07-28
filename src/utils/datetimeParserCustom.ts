e95ba67d0c255a883b78f5078c232bf8efb03fe4
Gustavo-Nasc <gustavo.ns@outlook.com.br>
2025-07-28 13:02:29 -0300

    chore: add timezone to 'datetimeParserCustom'

--- src/utils/datetimeParserCustom.ts
+++ src/utils/datetimeParserCustom.ts
@@ -1,7 +1,10 @@
 import { formatDate } from 'date-fns'
+import { ptBR } from 'date-fns/locale/pt-BR'
 
 export function datetimeParserCustom(date: Date) {
-  const formattedDate = formatDate(date, 'dd/MM/yyyy HH:mm')
+  const formattedDate = formatDate(date, 'dd/MM/yyyy HH:mm', {
+    locale: ptBR,
+  })
 
   return formattedDate
 }
